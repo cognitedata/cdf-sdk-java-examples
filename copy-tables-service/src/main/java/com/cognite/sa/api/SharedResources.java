6bdac76d2580bd5f301997fe1dbb12a16bacc65d
kjetilh33 <kjetil.halvorsen@cognite.com>
2021-03-14 22:31:03 +0100

    Add config handling

--- copy-tables-service/src/main/java/com/cognite/sa/api/SharedResources.java
+++ copy-tables-service/src/main/java/com/cognite/sa/api/SharedResources.java
@@ -1,9 +1,18 @@
 package com.cognite.sa.api;
 
 import com.cognite.client.CogniteClient;
+import com.google.cloud.secretmanager.v1.AccessSecretVersionRequest;
+import com.google.cloud.secretmanager.v1.AccessSecretVersionResponse;
+import com.google.cloud.secretmanager.v1.SecretManagerServiceClient;
+import com.google.cloud.secretmanager.v1.SecretVersionName;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.ws.rs.Produces;
+import java.io.IOException;
+import java.util.Optional;
 
 /**
  * This class hosts shared resources for all services.
@@ -14,6 +23,16 @@ import javax.ws.rs.Produces;
  */
 @ApplicationScoped
 public class SharedResources {
+    private static Logger LOG = LoggerFactory.getLogger(SharedResources.class);
+
+    @ConfigProperty(name = "cdf.key.env")
+    Optional<String> apiKey;
+
+    @ConfigProperty(name = "cdf.key.secret-manager")
+    Optional<String> secretUri;
+
+    @ConfigProperty(name = "cdf.base-url")
+    String baseURL;
 
     private CogniteClient client;
 
@@ -22,8 +41,61 @@ public class SharedResources {
         if (client == null) {
             // Instantiate the client
 
+
         }
 
         return client;
+    }
+
+    /*
+    Read secrets from GCP Secret Manager.
+    Since we are using workload identity on GKE, we have to take into account that the identity metadata
+    service for the pod may take a few seconds to initialize. Therefore the implicit call to get
+    identity may fail if it happens at the very start of the pod. The workaround is to perform a
+    retry.
+     */
+    private String getGcpSecret(String projectId, String secretId, String secretVersion) throws IOException {
+        int maxRetries = 3;
+        boolean success = false;
+        IOException exception = null;
+        String loggingPrefix = "getGcpSecret - ";
+        String returnValue = "";
+
+        for (int i = 0; i <= maxRetries && !success; i++) {
+            // Initialize client that will be used to send requests.
+            try (SecretManagerServiceClient client = SecretManagerServiceClient.create()) {
+                SecretVersionName name = SecretVersionName.of(projectId,
+                        secretId, secretVersion);
+
+                // Access the secret version.
+                AccessSecretVersionRequest request =
+                        AccessSecretVersionRequest.newBuilder().setName(name.toString()).build();
+                AccessSecretVersionResponse response = client.accessSecretVersion(request);
+                LOG.info(loggingPrefix + "Successfully read secret from GCP Secret Manager.");
+
+                returnValue = response.getPayload().getData().toStringUtf8();
+                success = true;
+            } catch (IOException e) {
+                String errorMessage = "Could not read secret from GCP secret manager. Will retry... " + e.getMessage();
+                LOG.warn(errorMessage);
+                exception = e;
+            }
+            if (!success) {
+                // Didn't succeed in reading the secret. Pause the thread to wait for the metadata service to start.
+                try {
+                    Thread.sleep(1000l);
+                } catch (Exception e) {
+                    LOG.warn("Not able to pause thread: " + e);
+                }
+            }
+        }
+
+        if (!success) {
+            // We didn't manage to read the secret
+            LOG.error("Could not read secret from GCP secret manager: {}", exception.toString());
+            throw exception;
+        }
+
+        return returnValue;
     }
 }
